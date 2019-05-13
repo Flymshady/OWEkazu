a01eaeaa43850533dfb3fbfe4560c5abbdbad2ec
Blickmeister <ondrablick@seznam.cz>
2019-05-13 20:40:30 +0200

    Pokus o personalData

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,6 @@
 export const appName = 'Aplikace pro LF';
 
-export const patientsUrl = 'https://owe2019.herokuapp.com/patient';
+export const patientsUrl = 'https://owe-kazu.herokuapp.com/api/rest/student';
 
 export const resultUrl = 'https://owe2019.herokuapp.com/result';
 
