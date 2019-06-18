b108c27666c9a664ddacfb5edea16d1a64e7ccce
Štěpán Cellar <stepan.cellar@gmail.com>
2019-06-18 19:39:40 +0200

    Pridani DiagnosisBar - rozhodnutí diagnozy u pacienta

--- src/constants/index.js
+++ src/constants/index.js
@@ -7,3 +7,5 @@ export const resultUrl = 'https://owe2019.herokuapp.com/result';
 export const loginUrl = 'https://owe2019.herokuapp.com/login';
 
 export const teacherUrl = 'https://owe-kazu.herokuapp.com/api/rest/teacher';
+
+export const diagnosisUrl = 'https://owe-kazu.herokuapp.com/api/rest/student/diagnosis';
