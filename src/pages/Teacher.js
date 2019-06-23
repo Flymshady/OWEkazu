76b8115f55f67f664159e04877ea82ab482f4e1c
Blickmeister <ondrablick@seznam.cz>
2019-06-24 00:16:18 +0200

    opraven edit diagnozy - mel by byt plne funkcni, odstraneni nektereho prebytecneho kodu

--- src/pages/Teacher.js
+++ src/pages/Teacher.js
@@ -1,36 +1,23 @@
 import React from 'react';
-import Section from '../components/Section';
 import {Form, NewDiagnosis, NewTemplate, EditDiagnosis} from '../components/Teacher';
-import {teacherUrl} from '../constants';
 
 class Teacher extends React.Component {
     state = {
-        teacher: {},
     };
-componentDidMount() {
-        fetch(teacherUrl)
-            .then((response) => response.json())
-            .then((jsonResponse) => {
-                this.setState({teacher: jsonResponse})
-            }).catch((err) => console.error(err));
-    }
-
 
     render() {
-        const {teacher} = this.state;
-const form = teacher.properties || [];
 
             return (
             <div class="teacher">
             <label class="Nadpis">Úpravy systému</label>
                <div className={'Content Grid'}>
-
                     <NewTemplate/>
-
                 </div>
+                <p></p>
                 <div className={'Content Grid'}>
                                    <NewDiagnosis/>
                 </div>
+                <p></p>
                  <div className={'Content Grid'}>
                   <EditDiagnosis/>
                  </div>
